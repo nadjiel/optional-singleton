d65483eefac2586463bc0317503e3a32cab425f9
Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
2024-07-25 10:56:10 -0300

    :books: docs: describing Dependency

--- src/Dependency.java
+++ src/Dependency.java
@@ -1,3 +1,15 @@
+/**
+ * This class is in this project only to serve as
+ * an representation of a dependency. So that in the
+ * OptionalSingleton implementation the dependency
+ * inversion principle can be shown.
+ * It is known that when following SOLID principles,
+ * classes should always strive to have abstractions
+ * so that the code can be the less coupled possible,
+ * but since this is a simple example and this class is
+ * extremely simple as well, it doesn't
+ * have any interfaces or anything to abstract it.
+ */
 public class Dependency {
 
     private final int id;
